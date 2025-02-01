5787400944e8b9f2163513d4e3d88b2ec1967a29
Shashanka Acharya <shasankaacharya14@gmail.com>
2025-02-01 09:27:29 +0545

    file-handling

---
+++ file-handling.py
@@ -0,0 +1,21 @@
+"""file=open("example.txt","w")
+file.write("hello world")
+file.close()
+
+file=open("example.txt","r")
+content=file.read()
+print(content)
+file.close()"""
+
+
+import json
+dict={
+    "name":"",
+    "age":10
+
+}
+with open("text.json","w")as f:
+    json.dump(dict,f)
+with open("text.json","r")as f:
+    loaded =json.load(f)
+print(loaded)
