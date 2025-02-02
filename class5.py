f82c9a4be96c72974799b7bb903d8307a0ed5f67
Shashanka Acharya <shasankaacharya14@gmail.com>
2025-02-02 08:48:09 +0545

    program using class & object

--- class5.py
+++ class5.py
@@ -1,5 +1,5 @@
 #class and object
-class Student:
+"""class Student:
     def __init__(self,fname,lname,age,address):
         self.fname=fname
         self.lname=lname
@@ -19,6 +19,46 @@ student1=Student('shasanka','Acharya',20,'kathmandu')
 student2=Student('bibek','Ghimire',19,'illam')
 print(student2.age)
 print(student1.get_name())
-print(student1.upper_name())
+print(student1.upper_name())"""
+
+#program to change address of student using class and object
+
+student=[]
+class A:
+    def __init__(self,name,address):
+        self.name=name
+        self.address=address
+    def get_name(self):
+        return self.name
+    def address_update(self,address):
+        self.address=address
+    def __repr__(self):
+       return (f"{self.name}{self.address}")
+
+num=int(input("Enter the range of number:"))
+for i in range(num):
+    c=input(f"Enter {i+1}th name : ")
+    d=input(f"Enter address:")
+    std=A(c,d)
+    student.append(std)
+    
+name=input("Enter the name to change address")
+for i in student:
+    print(i.name)
+    if  i.name == name:
+        new_add=input("Enter new address:")
+        i.address = new_add
+    else:
+        print("student not found")
+
+print(student)
+for i in student:
+    print(f"Name:{i.name},address:{i.address}")
+
+
+
+    
+
+
 
 
